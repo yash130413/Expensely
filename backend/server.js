440b0e8678661aa19dcbfa00f862326ee572251a
Yash ROhilla <yashrohilla1204@gmail.com>
2026-06-09 11:45:33 +0530

    update production cors

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || '*',
+  origin: process.env.FRONTEND_URL,
   credentials: true
 }));
 app.use(express.json({ limit: '50mb' }));
